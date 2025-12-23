d617d00f44a96f5c8bdd7c6339eb416f3ff59c58
Konoplev Anatolii <61531801+ahatornn@users.noreply.github.com>
2025-12-23 20:51:03 +0300

    [1] Сделал красиво
    * Сделал красиво
    
    * Исравил
    
    ---------
    
    Co-authored-by: Anatoly Konoplev <anatoly.konoplev@monopoly.su>

--- WinForms/WinFormsTest/Form1.Designer.cs
+++ WinForms/WinFormsTest/Form1.Designer.cs
@@ -35,6 +35,7 @@ partial class Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.Color.Red;
         ClientSize = new System.Drawing.Size(800, 450);
         Text = "Это главная форма";
         ResumeLayout(false);
